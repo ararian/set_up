a990e97a8f0e51a8c992b74be2965628e40419a2
ararian <arat.aaa.192.6ap@gmail.com>
2022-11-13 18:04:46 +0900

    README.mdの説明を整理、set_up.ps1の内容整理

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 # set_up
+* set_up.ps1を実行することで、開発に必要なソフトウェアを簡単にインストール可能
+* インストールするアプリケーションは随時要確認
 
-set_up.ps1を実行することで、開発に必要なソフトウェアを簡単にインストール可能
-インストールするアプリケーションは随時要確認
+# Requirement
+powershellが実行可能な環境
+
+# Usage
+* 管理者として実行すること
+* プロジェクトのダウンロード
+* powershellの実行権限を一時的に強化
+* スクリプトの実行
+
+```bash
+git clone https://github.com/ararian/set_up.git
+cd set_up
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+```
+
+# License
+"set_up" is under [MIT license](https://en.wikipedia.org/wiki/MIT_License).
--- set_up.ps1
+++ set_up.ps1
@@ -3,14 +3,11 @@ Disable-UAC
 
 # windowsにおけるソフトウェアインストールの自動化を行う
 
-# powershellの実行権限を一時的に強化
-Set-ExecutionPolicy Bypass -Scope Process -Force
-
+# Windows Exploler等の設定を行うツールのダウンロード
 # Install boxstarter and chocolatey simultaneously
 . { Invoke-WebRequest -useb https://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression; Get-Boxstarter -Force
 
-
-
+# Windows Exploler等の設定を行う
 # 拡張子表示
 Set-WindowsExplorerOptions -EnableShowFileExtensions
 # 隠しフォルダ表示
@@ -33,43 +30,37 @@ Set-WindowsExplorerOptions -EnableShowRecentFilesInQuickAccess
 Set-WindowsExplorerOptions -EnableShowFrequentFoldersInQuickAccess
 
 
+# 各種ソフトウェアのダウンロード・インストール
 
-winget install "Pycharm Community Edition" -h
-winget install Miniconda3 -h
-winget install --id  Microsoft.WindowsTerminal  -h
-winget install "Microsoft Visual Studio Code" -h
-code --install-extension hediet.vscode-drawio
-winget install gitkraken -h
-winget install --id Microsoft.VisualStudio.2019.Community -h
-winget install WinSCP -h
-winget install --id Discord.Discord -h
-winget install --id 7zip.7zip -h
-winget install --id Git.Git -h
-winget install --id Valve.Steam -h
-winget install --id Microsoft.Teams -h
-
+# minicondaのインストール
 # conda installを実行するための準備 conda scriptの環境設定の実施
+winget install Miniconda3 -h
 $path = [Environment]::GetEnvironmentVariable('Path', 'Machine')
 $newpath = $path + ';' + $Home + '\miniconda3' + ';' + $Home + '\miniconda3\Library\bin' + ';' + $Home + '\miniconda3\Scripts' + ';' + $Home + '\miniconda3\condabin'
 [Environment]::SetEnvironmentVariable("Path", $newpath, 'Machine')
 
+winget install --id  Microsoft.WindowsTerminal  -h
+winget install "Microsoft Visual Studio Code" -h
+code --install-extension hediet.vscode-drawio
+winget install "Pycharm Community Edition" -h
+winget install --id Microsoft.VisualStudio.2022.Community -h
+winget install WinSCP -h
+winget install --id 7zip.7zip -h
+winget install --id Git.Git -h
+winget install --id Microsoft.Teams -h
+winget install --id GitHub.GitHubDesktop -h
+
+winget install --id Valve.Steam -h
+winget install --id Discord.Discord -h
+
+
+#download & install  pytoruch, tensorflow and CUDA 
+winget install "NVIDIA CUDA Toolkit" -v 11.7 -h
 conda init powershell
-conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch -y
+conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
 conda install tensorflow -y
-winget install "NVIDIA CUDA Toolkit" -v 11.3 -h
 
 wsl --install
-
-# 再起動後の実行処理を設定
-$regRunOnceKey = "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
-$powerShell = (Join-Path $env:windir "system32\WindowsPowerShell\v1.0\powershell.exe")
-$script = "wsl --set-default-version 2"
-$restartKey = "Restart-And-RunOnce"
-
-Set-ItemProperty -path $regRunOnceKey -name $restartKey -value "$powerShell $script"
-
-Restart-Computer -Force
-
 
 Enable-UAC
 Enable-MicrosoftUpdate
